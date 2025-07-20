7dc0d30c3e4df4a1915546f3623fe573418c295c
Zigvert <dark133788@gmail.com>
2025-07-20 15:46:58 +0300

    Ready3.0

--- server/src/main/java/ru/practicum/shareit/ShareItApp.java
+++ server/src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -2,12 +2,16 @@ package ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ShareItApp {
-
 	public static void main(String[] args) {
-		SpringApplication.run(ShareItApp.class, args);
-	}
+		SpringApplication app = new SpringApplication(ShareItApp.class);
+		ConfigurableApplicationContext context = app.run(args);
 
+		Environment env = context.getEnvironment();
+		System.out.println("Datasource URL: " + env.getProperty("spring.datasource.url"));
+	}
 }
